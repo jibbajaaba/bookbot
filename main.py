4fb701acf0825a93db1421d31b9c383d27d1289f
Mark A McTague <mmctaguecoding@gmail.com>
2025-07-18 21:15:44 -0400

    refactored main() to take a sys.argv argument to a book and then run the report. if argument not the length of 2 then will exit with a code 1

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from stats import word_count, char_count, char_sort
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         contents = f.read()
     return contents
 
-frankenstein = "./books/frankenstein.txt"
+def main(book):
+    if len(book) == 2:
+        text = get_book_text(book[1])
+        char_dict = char_count(text)
 
-text = get_book_text(frankenstein)
-char_dict = char_count(text)
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(word_count(text))
+        print("--------- Character Count -------")
+        char_sort(char_dict)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(word_count(text))
-    print("--------- Character Count -------")
-    char_sort(char_dict)
 
-main()
+main(sys.argv)
